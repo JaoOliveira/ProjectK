81fd056e7461b32c6dd3ae650d9e9fc5cf0cb7bc
Joao Oliveira <lucas.maiahtm@gmail.com>
2022-07-27 01:01:07 -0300

    update route name

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,7 @@ const routes = express.Router();
 
 routes.post("/user", UserController.createAccount);
 routes.post('/user/login', UserController.enterAccount);
-routes.put('/user/editar', UserController.editAccount);
+routes.put('/user/edit', UserController.editAccount);
 routes.delete('/user/delete', UserController.deleteAccount);
 
 module.exports = routes;
